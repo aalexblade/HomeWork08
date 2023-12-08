c932352f9836b64c793fc9c869e021e6cb545b3a
aalexblade <79399963+aalexblade@users.noreply.github.com>
2023-12-08 20:32:31 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+//     < div class="gallery" >
+//     <a href="images/image1.jpg"><img src="images/thumbs/thumb1.jpg" alt="" title=""/></a>
+//     <a href="images/image2.jpg"><img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image"/></a>
+// </ >
+const markup = galleryItems.map(({ preview, original, description }) =>
+    `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+  
+`
+)
+
+gallery.insertAdjacentHTML('beforeend', markup.join(''))
+gallery.addEventListener('click', onClick)
+
+function onClick(evt) {
+    evt.preventDefault()
+}
+
+const lightbox = new SimpleLightbox('.gallery__item')
